d1999693f7a74e9999103c864494c0c1fd350522
Gabriel Erick <gabrielmmve13@gmail.com>
2025-08-13 16:33:01 -0300

    desafio

---
+++ projeto-inicial/desafio1.js
@@ -0,0 +1,18 @@
+alert('Boas vindas ao nosso site!');
+
+let nome = 'Lua';
+let idade = 25;
+let numeroVendas = 50;
+let saldoDisponivel = 1000;
+
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+let novoNome = prompt('Digite seu nome:');
+idade = prompt('Digite a sua idade:');
+
+if (idade >= 18) {
+    alert('Pode tirar a habilitação.');
+} else {
+    alert('Não pode tirar a habilitação.');
+}
