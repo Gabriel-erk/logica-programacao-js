a88d9bef8964d5814a249904f31bf10129eb0928
Gabriel Erick <gabrielmmve13@gmail.com>
2025-08-13 17:20:36 -0300

    else e laço de repetição

--- projeto-inicial/app.js
+++ projeto-inicial/app.js
@@ -1,10 +1,22 @@
 alert('Boas vindas ao jogo do número secreto!'); 
 // let numeroSecreto = parseInt(Math.random() * 11);
 let numeroSecreto = 29;
+console.log(numeroSecreto);
 let palpite = prompt('Escolha um número entre 1 e 30');
 
 if (palpite == numeroSecreto) {
-    console.log('Parabéns! Você acertou o número secreto: ' + numeroSecreto);
+    alert('Parabéns! Você acertou o número secreto: ' + numeroSecreto);
 } else {
-    alert('Que pena! O número secreto era ' + numeroSecreto);
+    alert('Você errou :( Tente novamente!');
+    while (palpite != numeroSecreto) {
+        palpite = prompt('Escolha um número entre 1 e 30');
+        if (palpite == numeroSecreto) {
+            alert('Parabéns! Você acertou o número secreto: ' + numeroSecreto);
+            break;
+        } else if (palpite < numeroSecreto) {
+            alert('O número secreto é maior que ' + palpite);
+        } else {
+            alert('O número secreto é menor que ' + palpite);
+        }
+    }
 }
