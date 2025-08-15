2a42034b2b1f99bc30346eb6bf29f30a8bb406f7
Gabriel Erick <gabrielmmve13@gmail.com>
2025-08-14 23:27:31 -0300

    math.random

--- projeto-inicial/app.js
+++ projeto-inicial/app.js
@@ -1,6 +1,6 @@
 alert("Boas vindas ao jogo do número secreto!");
 // let numeroSecreto = parseInt(Math.random() * 11);
-let numeroSecreto = 29;
+let numeroSecreto = parseInt(Math.random() * 10 + 1); // multiplica por 10 pois o número gerado pelo math.random é maior que zero e menor que um, ou seja, apenas entrega números decimais como: 0.5643, 0.3425, 0.2525 etc e com a multiplicação pegamos apenas a parte inteira desse valor, movendo uma casa da vírgula, então, se me gerar: 0.4564, devido a multiplicação por 10, terei 4.564 onde o parseInt irá remover essa parte decimal para mim depois obs: somo com um pois ai teria apeans um intervalo entre 1 e 9, já que na documentação dessa função diz que o último número (no caso o 10) não é incluido, então, após a soma seria como multiplicar por 11, porém o intervalo vai corretamente de 1 a 10 agora (caso queira com 100, 200, só alterar o número que está multiplicando, ex abaixo: entre 1 e 100: let numeroSecreto = parseInt(Math.random() * 100 + 1))
 let palpite;
 console.log(numeroSecreto);
 let tentativas = 0;
