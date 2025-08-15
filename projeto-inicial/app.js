2383fc8e754020529f8a61917b99b938a6b573ae
Gabriel Erick <gabrielmmve13@gmail.com>
2025-08-14 23:12:40 -0300

    break

--- projeto-inicial/app.js
+++ projeto-inicial/app.js
@@ -8,9 +8,6 @@ let tentativas = 0;
 while (palpite != numeroSecreto) {
   palpite = prompt("Escolha um número entre 1 e 30");
   if (palpite == numeroSecreto) {
-    alert(
-      `Parabéns! Você acertou o número secreto: ${numeroSecreto} com ${tentativas} tentativas`
-    );
     break;
   } else {
     if (palpite < numeroSecreto) {
@@ -18,7 +15,17 @@ while (palpite != numeroSecreto) {
     } else {
       alert(`O número secreto é menor que ${palpite}`);
     }
+    // fora do if/else acima pois se chegou até aqui é porque ele tentou e não acertou, já que, se tivesse, teria caido no break e o código teria se encerrado sem chegar até aqui
+    tentativas++;
   }
-  // fora do if/else pois se chegou até aqui é porque ele tentou e não acertou, já que, se tivesse, teria caido no break e o código teria se encerrado sem chegar até aqui
-  tentativas++;
+}
+
+if (tentativas > 1) {
+  alert(
+    `Parabéns! Você acertou o número secreto: ${numeroSecreto} com ${tentativas} tentativas`
+  );
+} else {
+  alert(
+    `Parabéns! Você acertou o número secreto: ${numeroSecreto} com ${tentativas} tentativa`
+  );
 }
